9cae175ed184b160561fc01508d334f3eb65b2a2
Zach <streetbikeford@gmail.com>
2015-01-28 10:28:55 -0600

    TryExcept
    Gives useful user feedback to errors in configuration

---
+++ TryExcept.py
@@ -0,0 +1,28 @@
+##Try statements allow you to give user error messages
+
+##tuples are basically read-only lists
+
+##You specify a tuple. You TRY to append a value to the tuple. If what your trying does not work
+##Python will run your except statement, which in this cause prints "Configuration Error".
+##If what you try is successful, Python will skip except, and move onto your else statement.
+
+tuple=(1,4,54,55)
+
+try:
+	tuple.append(45) ##If you just type tuple.append, this code will error out. 
+except:
+	print('Configuration Error')
+else:
+	for each in tuple:
+		print(each)
+
+## You can also move the else statement to follow the try and the code will run the same
+
+tuple=(1,4,54,55)
+
+try:
+	tuple.append(45)      ## Your script will try to append the tuple, fail, and skip to except
+	for each in tuple:
+		print(each)
+except:
+	print('Configuration Error')
