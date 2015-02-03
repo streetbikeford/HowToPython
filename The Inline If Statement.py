0473c7a2fbe5fe2837bd2f22e78ad1f67218dc40
Zach <streetbikeford@gmail.com>
2015-02-03 14:57:06 -0600

    InlineIf and Datatypes

---
+++ The Inline If Statement.py
@@ -0,0 +1,14 @@
+## A typical if statement will look something like this
+
+a, b = 25, 35
+
+if a >= b: 
+	print(True)
+else:
+	print(False)
+
+## You could also write this statement as 
+
+print(True if a>=b else False)
+
+## Both will run exactly the same, the inline If is just easier on the eyes. 
