463e62aef19f68243305af89620f6043f70bec7c
Zach <streetbikeford@gmail.com>
2015-02-03 12:46:48 -0600

    Adding Comparison operators

--- Operators.py
+++ Operators.py
@@ -17,3 +17,18 @@ print(a ** b, 'This is the value of a to the power of b')
 
 
 ##Comparison Operators
+
+c = 2
+d = 4
+
+e = 'Hello World'
+f = 'Hello World'
+
+print (c == d, 'This checks if the two variables are equal')
+print (e == f, "This also works with strings")
+
+print (c != d, 'This checks if the two variables are NOT equal')
+print (c > d, 'This checks if the left operands is greater than the right')
+print (c < d, 'This checks if the right operands is greater than the left')
+print (c >= d, 'This checks if the left operands is greater than or equal to the right')
+print (c <= d, 'This checks if the right operands is greater than or equal to the left')
