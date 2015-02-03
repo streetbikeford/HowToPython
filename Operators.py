1c1da9a4e782f4f04b8c8254c53bc8b42bba22eb
Zach <streetbikeford@gmail.com>
2015-02-03 12:36:29 -0600

    Operators worksheet

---
+++ Operators.py
@@ -0,0 +1,19 @@
+## Operators come in many varieties. Arithmetic, logical, assignment, comparison
+##identity, etc.  
+
+##Basic arithmetic operators. Begin by assigning 2 variables a value
+
+a = 35
+b = 25
+
+
+
+print(a + b, 'This is the value of a plus b')
+print(a - b, 'This is the value of a minus b')
+print(a * b, 'This is the value of a times b')
+print(a / b, 'This is the value of a divided by b')
+print(a % b, 'This is the value of a divided b with remainder')
+print(a ** b, 'This is the value of a to the power of b')
+
+
+##Comparison Operators
